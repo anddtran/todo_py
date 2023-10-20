dcd40f9293ada85191375738e9755f1d4bec685a
anddtran <anddtran@gmail.com>
2023-10-19 19:15:02 -0500

    added icon and moved things around

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 #author: Andrew Tran
 
 import tkinter as tk            # importing the tkinter module as tk  
+from tkinter import *
 from tkinter import messagebox  # importing the messagebox module from the tkinter library  
+from PIL import ImageTk, Image  
 import os     
 
 tasks = []
@@ -71,31 +73,40 @@ def create_popup():
     confirm_button.pack()
 
 if __name__ == '__main__':
-    #create the window
+    # Create the window
     root = tk.Tk()
     root.title('Todo')
-    root.geometry('400x600')
-    root.resizable(False, False) 
+    root.geometry('400x600')  # Consider making the window a bit larger if needed
+    root.resizable(False, False)
 
-    #Title
-    label = tk.Label(root, text='List')
-    label.pack()
+    # Background image
+    bgimage = Image.open('ds.png')
+    bgresize = bgimage.resize((50, 50), Image.NEAREST)
+    tkimage = ImageTk.PhotoImage(bgresize)
+    tkbg = tk.Label(root, image=tkimage)
+    tkbg.place(relx=0.5, rely=0.1, anchor='center')  # Adjusted position
 
-    #listbox widget
-    listbox = tk.Listbox(root, width=30, height=30)
-    listbox.pack()
+    # Title
+    label = tk.Label(root, text='List', fg= 'white')
+    label.place(relx=0.5, rely=0.18, anchor='center')  # Moved down slightly
+    
+
+    # Listbox widget
+    listbox = tk.Listbox(root, width=30, height=20)  # Reduced height to fit buttons
+    listbox.place(relx=0.5, rely=0.5, anchor='center')  # Center of the window
+
     listbox.focus_set()
     listbox.bind("<Key>", on_keypress)
     readtasks()
     update_listbox()
 
-    #add task button
+    # Add task button
     button = tk.Button(root, text='Add Task', command=create_popup)
-    button.pack()
+    button.place(relx=0.3, rely=0.85, anchor='center')  # Moved up slightly
 
-    #add delete button
+    # Add delete button
     deleteitem = tk.Button(root, text='Delete', command=deletetask)
-    deleteitem.pack()
+    deleteitem.place(relx=0.7, rely=0.85, anchor='center')  # Moved up slightly
 
-
+    label.lift()
     root.mainloop()
